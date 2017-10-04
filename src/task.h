02e3ae993c35c7cfc93570041b4b0b124f4c241a
Positron <positron@rogers.com>
2017-10-04 11:33:15 -0400

    Publish namespace separators based on the declaration

--- src/task.h
+++ src/task.h
@@ -1367,5 +1367,6 @@ struct include_history_entry* t_alloc_include_history_entry(
 struct include_history_entry* t_decode_include_history_entry(
    struct task* task, int id );
 struct script* t_alloc_script( void );
+struct ns* t_find_ns_of_object( struct task* task, struct object* object );
 
 #endif
