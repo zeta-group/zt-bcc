065394ff2fbe8a6ed3d903d0f6530e67a5fa2c79
Moises Aguirre <aguirremoises@proton.me>
2025-03-08 20:32:25 -0400

    Change non-private initializer warning to only occur on module references.

--- src/semantic/dec.c
+++ src/semantic/dec.c
@@ -1432,9 +1432,9 @@ static bool test_scalar_initz( struct semantic* semantic,
    // Note which variable is passed by reference.
    if ( s_is_ref_type( &test->initz_type ) ) {
       if ( expr.var ) {
-         if ( ! expr.var->hidden ) {
+         if ( (! expr.var->hidden) && (((struct ref_array*)expr.var->ref)->storage == STORAGE_MAP) ) {
             s_diag( semantic, DIAG_POS_ERR, &value->expr->pos,
-               "non-private initializer (references only work with private "
+               "non-private initializer (module references only work with private "
                "map variables)" );
             s_bail( semantic );
          }
